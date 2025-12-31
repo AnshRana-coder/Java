54012dd325d0e59358264a0e8fd54d5524258140
AnshRana-coder <Anshjatrana555@gmail.com>
2025-12-31 20:36:07 +0530

    new commmit

--- Array/3sum.java
+++ Array/3sum.java
@@ -8,6 +8,7 @@ public class 3sum {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
+            
             int right = nums.length - 1;
 
             while (left < right) {
